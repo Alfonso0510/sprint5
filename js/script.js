917f9ecf1043748df092a655363226642d10719c
Alfonso Vacas <alfonsovacas1979@gmail.com>
2022-03-12 18:31:59 +0100

    Ejercicio 3 evaluar chistes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const URL_API = "https://icanhazdadjoke.com/slack";
 //Constante para imprmir por pantalla el chiste, usando selector de clase
 const tmpl = document.querySelector(".joke");
+let ratedJokes = []
 
 //Exercici 1 mostrar por consola
 // function showJoke(){
@@ -20,4 +21,18 @@ function showJoke() {
         //Pintamos por pantalla el chiste
         tmpl.innerHTML = `${data.attachments[0].text}`
     })
+}
+
+function rateJoke(rate){
+    //Creamos el objeto joke para guardar
+    let ratedJoke = {};
+    //Introducimos el texto del chiste
+    ratedJoke.joke = tmpl.innerHTML;
+    //Introducimos la puntuacion
+    ratedJoke.score = rate;
+    //Creamos una current date
+    ratedJoke.date = new Date();
+    //Hacemos un push al array de jokes
+    ratedJokes.push(ratedJoke);
+    console.log("reportJokes: ", ratedJokes)
 }
