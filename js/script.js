f98a568b19457dbfddb1a3aa1e7ed257aa8582a1
Alfonso Vacas <alfonsovacas1979@gmail.com>
2022-03-12 18:28:54 +0100

    Ejercicio 2 mostrar por pantalla

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 ////Constante donde guardamos la direccion de la api
 const URL_API = "https://icanhazdadjoke.com/slack";
+//Constante para imprmir por pantalla el chiste, usando selector de clase
+const tmpl = document.querySelector(".joke");
 
 //Exercici 1 mostrar por consola
-function showJoke(){
-     fetch(URL_API)
-     .then((response) => response.json())
-     .then((data) => {
-         console.log(data.attachments[0].text);
-     })
- };
+// function showJoke(){
+//      fetch(URL_API)
+//      .then((response) => response.json())
+//      .then((data) => {
+//          console.log(data.attachments[0].text);
+//      })
+//  };
+
+//Exercici 2 mostrar por pantalla
+function showJoke() {
+    fetch(URL_API)
+    .then(response => response.json())
+    .then((data) => {
+        //Pintamos por pantalla el chiste
+        tmpl.innerHTML = `${data.attachments[0].text}`
+    })
+}
