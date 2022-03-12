829c85ac2f3395f4dcbb8639bba525f48abe8e28
Alfonso Vacas <alfonsovacas1979@gmail.com>
2022-03-12 19:54:15 +0100

    Ejercicio 4 api meteo

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ const URL_API = "https://icanhazdadjoke.com/slack";
 const tmpl = document.querySelector(".joke");
 let ratedJokes = []
 
+//Wheaher variables
+let lon;
+let lat;
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+const kelvin = 273;
+
 //Exercici 1 mostrar por consola
 // function showJoke(){
 //      fetch(URL_API)
@@ -16,14 +25,15 @@ let ratedJokes = []
 //Exercici 2 mostrar por pantalla
 function showJoke() {
     fetch(URL_API)
-    .then(response => response.json())
-    .then((data) => {
-        //Pintamos por pantalla el chiste
-        tmpl.innerHTML = `${data.attachments[0].text}`
-    })
+        .then(response => response.json())
+        .then((data) => {
+            //Pintamos por pantalla el chiste
+            tmpl.innerHTML = `${data.attachments[0].text}`
+        })
 }
 
-function rateJoke(rate){
+//Exercici 3 valorar chiste
+function rateJoke(rate) {
     //Creamos el objeto joke para guardar
     let ratedJoke = {};
     //Introducimos el texto del chiste
@@ -36,3 +46,40 @@ function rateJoke(rate){
     ratedJokes.push(ratedJoke);
     console.log("reportJokes: ", ratedJokes)
 }
+
+
+//Exercici 4 mostrar api meteorologica
+window.addEventListener("load", () => {
+    //Geolocalizacion
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            // Token de la API
+            const api = "033365ec6e62260e2f094aaae64e894c";
+
+            // URL de la API
+            const base =
+                `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+                `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
+
+            //LLamada a la API
+            fetch(base)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    temperature.textContent =
+                        Math.floor(data.main.temp - kelvin) + "°C";
+                    summary.textContent = data.weather[0].description;
+                    loc.textContent = data.name + "," + data.sys.country;
+                    let icon1 = data.weather[0].icon;
+                    icon.innerHTML =
+                        `<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
+                });
+        });
+    }
+});
